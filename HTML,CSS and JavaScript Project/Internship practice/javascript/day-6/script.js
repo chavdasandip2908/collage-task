4c49ee51ebc134789183bf6e07814b746cb0833b
sandip <chavda@pradip>
2023-02-24 08:48:53 +0530

    JavaScript Task And Validation

---
+++ HTML,CSS and JavaScript Project/Internship practice/javascript/day-6/script.js
@@ -0,0 +1,7 @@
+// alert("hello");
+console.log(window);
+console.log(document.body);
+// document.body.style.backgroundColor="green;
+ 
+window.alert("window alert!");
+window.prompt("enter value =>");
